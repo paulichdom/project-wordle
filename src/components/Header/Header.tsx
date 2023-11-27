3886733b59c2672d03a07a7db508e585becf3bc0
Domagoj <paulic.domagoj@gmail.com>
2023-11-27 22:09:39 +0100

    style header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,9 @@ import styles from './Header.module.css'
 
 export const Header = () => {
   return (
-    <header className={styles.wrapper}>Header</header>
+    <header className={styles.header_wrapper}>
+      <h1>Word Game</h1>
+      </header>
   )
 }
 

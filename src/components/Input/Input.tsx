ad8e050e16a9e4d5e191590a9f9c55ab5a0e9ac4
Domagoj <paulic.domagoj@gmail.com>
2023-11-29 09:30:54 +0100

    control react state, log user input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,15 +1,30 @@
+import { FormEvent, useState } from 'react';
 import styles from './Input.module.css';
 
 function Input() {
+  const [userGuess, setUserGuess] = useState<string>('');
+
+  const handleSubmitAction = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log({ userGuess });
+  };
+
   return (
-    <form className={styles.guessInputForm}>
+    <form
+      className={styles.guessInputForm}
+      onSubmit={(event) => handleSubmitAction(event)}
+    >
       <label htmlFor="guess-input" className={styles.guessLabel}>
         Enter guess:
         <input
           className={styles.inputField}
           id="guess-input"
           type="text"
-          placeholder="email@domain.com"
+          placeholder=""
+          value={userGuess}
+          onChange={(event) => {
+            setUserGuess(event.target.value);
+          }}
         />
       </label>
     </form>

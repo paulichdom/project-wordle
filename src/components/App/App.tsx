5672c2eafa546c8f65caf1bf3c3a6fb12ef72a36
Domagoj <paulic.domagoj@gmail.com>
2024-10-28 09:13:39 +0100

    add helper function

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,11 @@
 import { PageLayout } from '../PageLayout';
 import Game from '../Game';
+import { checkGuess } from '../../game-helpers';
 
 export const App = () => {
+  const checkedGuesses = checkGuess('WHALE', 'LEARN');
+
+  console.log({checkedGuesses})
   return (
       <PageLayout>
         <Game />
---
+++ src/game-helpers.ts
@@ -0,0 +1,31 @@
+type GuessStatus = 'correct' | 'incorrect' | 'misplaced';
+
+type Guess = {
+  letter: string;
+  status: GuessStatus;
+};
+
+export const checkGuess = (
+  userGuess: string,
+  correctAnswer: string
+): Guess[] => {
+  const checkedGuesses: Guess[] = [];
+
+  [...userGuess].forEach((guessLetter, guessIndex) => {
+    if ([...correctAnswer].includes(guessLetter)) {
+      [...correctAnswer].forEach((answerLetter, answerIndex) => {
+        if (guessLetter === answerLetter && guessIndex === answerIndex) {
+          checkedGuesses.push({ letter: guessLetter, status: 'correct' });
+        }
+
+        if (guessLetter === answerLetter && guessIndex !== answerIndex) {
+          checkedGuesses.push({ letter: guessLetter, status: 'misplaced' });
+        }
+      });
+    } else {
+      checkedGuesses.push({ letter: guessLetter, status: 'incorrect' });
+    }
+  });
+
+  return checkedGuesses;
+};
